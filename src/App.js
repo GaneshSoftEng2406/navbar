a96fe36496c963d28709c834e0d1ee8eb20f50f2
Ganesh Aher <aherganesh194@gmail.com>
2022-05-18 09:52:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Nav from './components/navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Nav />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Nav = () =>{
+    return <div className='main'>
+        <div className='parent'>
+  <div id='first'>
+<p id='mor'>LOGOBAKERY</p>
+<p>Service</p>
+<p>Project</p>
+<p>About</p>
+  </div>
+
+  <div id='second'>
+<button>Contact</button>
+  </div>
+
+
+
+        </div>
+
+    </div>
+};
+export default Nav;
