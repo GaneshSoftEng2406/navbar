7f25de464cba6c3c0ad12232b4680c0e15e820c9
Ganesh Aher <aherganesh194@gmail.com>
2022-05-18 13:19:52 +0530

    first commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,16 +4,15 @@ const Nav = () =>{
     return <div className='main'>
         <div className='parent'>
   <div id='first'>
-<p id='mor'>LOGOBAKERY</p>
-<p>Service</p>
-<p>Project</p>
-<p>About</p>
+<a href='#' id='mor'>LOGOBAKERY</a>
+<a href='#'>Service</a>
+<a href='#'>Project</a>
+<a href='#'>About</a>
   </div>
 
   <div id='second'>
-<button>Contact</button>
+<button href='#'>Contact</button>
   </div>
-
 
 
         </div>
